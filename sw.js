e50725b8c99e3362915d29659e5ba8592ea402b0
uzelFR <a.boudoux@protonmail.com>
2025-08-13 05:29:26 +0200

    Ajout initial de la PWA Arkhès

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'arkhes-cache-v1';
+const FILES_TO_CACHE = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'sw.js',
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => {
+      return resp || fetch(evt.request);
+    })
+  );
+});
